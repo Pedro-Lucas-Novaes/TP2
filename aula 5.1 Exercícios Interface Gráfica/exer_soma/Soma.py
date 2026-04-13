1b31870813992582696b3e586352183452f1eeb6
DSM3 <fatec-dsm3@academico.intra>
2026-04-13 18:59:09 -0300

    att

--- aula 5.1 Exercícios Interface Gráfica/exer_soma/Soma.py
+++ aula 5.1 Exercícios Interface Gráfica/exer_soma/Soma.py
@@ -1,18 +1,39 @@
 from tkinter import *
 
-class Clientes:
+class Soma:
     def __init__(self):
         self.tela = Tk()
         self.configurar_tela()
         self.criar_componentes()
 
         #Atributos Privados
-        self.__nome = ""
-        self.__email = ""
-        self.__telefone = ""
-        self.__endereco = ""
+        self.__n1 = 0
+        self.__n2 = 0
+        self.__soma = 0
 
-    
+    @property
+    def _n1(self):
+        return self.__n1
+
+    @_n1.setter
+    def _n1(self, value):
+        self.__n1 = value
+
+    @property
+    def _n2(self):
+        return self.__n2
+
+    @_n2.setter
+    def _n2(self, value):
+        self.__n2 = value
+
+    @property
+    def _soma(self):
+        return self.__soma
+
+    @_soma.setter
+    def _soma(self, value):
+        self.__soma = value
 
 
     def configurar_tela(self):
@@ -42,48 +63,44 @@ class Clientes:
         self.frame.pack(expand=True)
 
         #Titulo
-        self.titulo = Label(self.frame, text="Cadastro de Clientes")
+        self.titulo = Label(self.frame, text="Soma de Numeros")
         #grid => cria grade  row = linha colum = colunas columnspan = espaço interno coluna
         #pady = espaçamento parte de cima e de baixo de 10px
         self.titulo.grid(row= 0, column= 0, columnspan= 1, pady= 10 )
 
-        
+        #numero1
 
         #sticky = "w" posicionamento do texto lado esquedo (oeste)
-        Label(self.frame, text="Nome: ").grid(row=1, column= 0, sticky= "w", pady= 5)
+        Label(self.frame, text="Numero 1: ").grid(row=1, column= 0, sticky= "w", pady= 5)
 
-        self.txt_nome = Entry(self.frame)
-        self.txt_nome.grid(row= 1, column= 1, pady= 5)
+        self.txt_n1 = Entry(self.frame)
+        self.txt_n1.grid(row= 1, column= 1, pady= 5)
 
-        
-        Label(self.frame, text="email: ").grid(row=3, column= 0, sticky= "w", pady= 5)
+        #Numero2
+        Label(self.frame, text="Numero 2: ").grid(row=3, column= 0, sticky= "w", pady= 5)
 
-        self.txt_email = Entry(self.frame)
-        self.txt_email.grid(row= 3, column= 1, pady= 5)
-
-        Label(self.frame, text="telefone: ").grid(row=4, column= 0, sticky= "w", pady= 5)
-
-        self.txt_telefone = Entry(self.frame)
-        self.txt_telefone.grid(row= 4, column= 1, pady= 5)
-
-        Label(self.frame, text="endereço: ").grid(row=5, column= 0, sticky= "w", pady= 5)
-
-        self.txt_endereco = Entry(self.frame)
-        self.txt_endereco.grid(row= 5, column= 1, pady= 5)
+        self.txt_n2 = Entry(self.frame)
+        self.txt_n2.grid(row= 3, column= 1, pady= 5)
 
         #Resultado
 
-        Label(self.frame, text="Resultado: ").grid(row=6, column= 0, sticky= "w", pady= 5)
+        Label(self.frame, text="Resultado: ").grid(row=4, column= 0, sticky= "w", pady= 5)
 
         self.txt_resul = Entry(self.frame)
-        self.txt_resul.grid(row= 6, column= 1, pady= 5)
-        
+        self.txt_resul.grid(row= 4, column= 1, pady= 5)
 
         #Botao
-        self.btn_botao = Button(self.frame, text="Cadastrar Cliente",command= self.calcular)
-        self.btn_botao.grid(row = 8, column= 0, columnspan= 2, pady= 15)
+        self.btn_botao = Button(self.frame, text="Calcular",command= self.calcular)
+        self.btn_botao.grid(row = 5, column= 0, columnspan= 2, pady= 15)
 
-    
+    def calcular(self):
+        #Recebendo os valores das caixas de texto e guardando atributos
+        self.__n1 = float(self.txt_n1.get())
+        self.__n2 = float(self.txt_n2.get())
+        self.__soma = self.__n1 + self.__n2
+
+        #Colocar o resultado da soma na caixa de texto txt_resul
+        self.txt_resul.insert(0,self.__soma)
         
     def executar(self):
         self.tela.mainloop()
