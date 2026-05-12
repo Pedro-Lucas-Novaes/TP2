7aa2361c638aab09de8ccd9e5b10e982d28ec0b1
Pedro-Lucas-Novaes <pedrolucas.novaes.dutra@gmail.com>
2026-05-12 20:38:55 -0300

    att

---
+++ aula_08_db_mongo/mecanica/Principal.py
@@ -0,0 +1,11 @@
+from Mecanica import Mecanica
+
+class Principal:
+
+    @staticmethod
+    def main():
+        cli = Mecanica()
+        cli.executar()
+
+if __name__ == "__main__":
+    Principal.main()
